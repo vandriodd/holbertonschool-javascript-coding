038be35179e0185aa86b0c8e287cef431abb1a88
vandriodd <ludasaadi@gmail.com>
2023-09-19 20:10:42 +0000

    Loripsum

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const content = process.argv[3];
+
+request(url, (error, response, body) => {
+  fs.writeFile(content, body, 'utf-8', () => {});
+});
