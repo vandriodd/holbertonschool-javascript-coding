0739f80c616200b5785f42f2509dcc419fe96fa3
vandriodd <ludasaadi@gmail.com>
2023-09-19 20:19:35 +0000

    Fix semistandard validation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,5 +7,7 @@ const url = process.argv[2];
 const content = process.argv[3];
 
 request(url, (error, response, body) => {
+  if (error) console.log(error);
+  // console.log(response.statusCode);
   fs.writeFile(content, body, 'utf-8', () => {});
 });
