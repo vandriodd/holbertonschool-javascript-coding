5742fe98f9ae08649b710e1573b1936553f49cb5
vandriodd <ludasaadi@gmail.com>
2023-09-19 20:17:11 +0000

    Fix semistandard validation

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -8,7 +8,7 @@ const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 request(url, (error, response, body) => {
   if (error) console.log(error);
 
-  //console.log(`code: ${response.statusCode}`);
+  // console.log(`code: ${response.statusCode}`);
 
   const data = JSON.parse(body);
   const title = data.title;
