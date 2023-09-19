d8b218cc59bc02e51b9833735c81442163461328
vandriodd <ludasaadi@gmail.com>
2023-09-19 19:48:15 +0000

    Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request(url, (error, response, body) => {
+  //if (error) console.log(error);
+  //console.log(`code: ${response.statusCode}`);
+  const data = JSON.parse(body);
+  const title = data.title;
+  console.log(title);
+});
