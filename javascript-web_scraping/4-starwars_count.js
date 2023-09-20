a7d9b0b7411464ceacae9037e7d2cbd28480c24f
vandriodd <ludasaadi@gmail.com>
2023-09-20 19:11:20 +0000

    Star wars Wedge Antilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  // console.log(response.statusCode);
+
+  const data = JSON.parse(body).results;
+  let count = 0;
+
+  data.filter((movie) => {
+    if (movie.characters.includes(`https://swapi-api.hbtn.io/api/people/18/`))
+      count++;
+  });
+  console.log(count);
+});
