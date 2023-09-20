a8d6ba4f579fd0c4751026c2fedea692fbda2c0d
vandriodd <ludasaadi@gmail.com>
2023-09-20 19:27:12 +0000

    Fix semistandard validation [2]

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,7 +12,7 @@ request(url, (error, response, body) => {
   let count = 0;
 
   data.filter((movie) => {
-    if (movie.characters.includes(`https://swapi-api.hbtn.io/api/people/18/`)) {
+    if (movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
       count++;
     }
   });
